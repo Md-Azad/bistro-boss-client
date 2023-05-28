0e0e8adb3b1b222dc9a007f4daa69a256ebc8f2e
Md-Azad <sokalazad@gmail.com>
2023-05-29 01:37:50 +0200

    added react helmet to get dynamic title

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import PopularMenu from "../../../Shared/PopularMenu/PopularMenu";
 import Banner from "../../Banner/Banner";
 import Category from "../../Category/Category";
@@ -10,6 +11,9 @@ import Testimonials from "./Testimonials/Testimonials";
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
       <Banner></Banner>
       <Category></Category>
       <Notice></Notice>
