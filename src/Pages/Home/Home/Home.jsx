d7248ef9250d5e672f9dbb35074c6e3447980aaf
Md-Azad <sokalazad@gmail.com>
2023-05-28 17:30:20 +0200

    added another page named notice under category component

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Banner/Banner";
 import Category from "../../Category/Category";
+import Notice from "../../Notice/Notice";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      <Notice></Notice>
     </div>
   );
 };
---
+++ src/Pages/Notice/Notice.jsx
@@ -0,0 +1,22 @@
+// import notice from "../../assets/assets/home/chef-service.jpg";
+import "./Notice.css";
+
+const Notice = () => {
+  return (
+    <div className="bg-url flex justify-center items-center my-12 ">
+      <div className="bg-white w-3/4 h-80  ">
+        <div className=" w-3/4 mx-auto my-20">
+          <h1 className="text-center text-4xl">Bistro Boss</h1>
+          <p className="text-xl text-center">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+            veniam explicabo nesciunt dolore quidem quia itaque? Voluptatum nam
+            cupiditate repudiandae autem accusantium in cum, tenetur veritatis
+            excepturi illum est explicabo!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notice;
