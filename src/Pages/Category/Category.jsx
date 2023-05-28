597a3c581c49a87eb2edf9ae34c929244a23a913
Md-Azad <sokalazad@gmail.com>
2023-05-28 16:23:12 +0200

    added footer, navbar, banner,and category

---
+++ src/Pages/Category/Category.jsx
@@ -0,0 +1,58 @@
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import slide1 from "../../assets/assets/home/slide1.jpg";
+import slide2 from "../../assets/assets/home/slide2.jpg";
+import slide3 from "../../assets/assets/home/slide3.jpg";
+import slide4 from "../../assets/assets/home/slide4.jpg";
+import slide5 from "../../assets/assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <Swiper
+      slidesPerView={4}
+      spaceBetween={30}
+      centeredSlides={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper mb-24"
+    >
+      <SwiperSlide>
+        <img src={slide1} alt="" />
+        <p className="uppercase text-center -mt-12 text-3xl text-white">
+          salads
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide2} alt="" />
+        <p className="uppercase text-center -mt-12 text-3xl text-white">
+          pizzas
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide3} alt="" />
+        <p className="uppercase text-center -mt-12 text-3xl text-white">
+          soups
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide4} alt="" />
+        <p className="uppercase text-center -mt-12 text-3xl text-white">
+          desserts
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide5} alt="" />
+        <p className="uppercase text-center -mt-12 text-3xl text-white">
+          salads
+        </p>
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Category;
